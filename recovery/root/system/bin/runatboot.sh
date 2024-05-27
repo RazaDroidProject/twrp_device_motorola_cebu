9231d3c4ca0679357cbe24741ddf43c107cbef0e
MD Raza <delightreza@gmail.com>
2024-05-28 01:16:38 +0530

    cebu: Bringup For FBEv2
    Signed-off-by: MD Raza <delightreza@gmail.com>
    Change-Id: I44a67c30d2de036c0511adc00577986b8636be98

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,39 +1,49 @@
 #!/system/bin/sh
 
-module_path=/vendor/lib/modules
-
 touch_class_path=/sys/class/touchscreen
 touch_path=
 firmware_path=/vendor/firmware
 firmware_file=
+device=$(getprop ro.boot.device)
 
-# Load TouchScreen Modules
-insmod $module_path/himax_v3_mmi_hx83102d.ko
-insmod $module_path/himax_v3_mmi.ko
-insmod $module_path/ili9882_mmi.ko
-insmod $module_path/chipone_tddi_mmi.ko
-insmod $module_path/nova_0flash_mmi.ko
-
-# Load Needed Modules
-insmod $module_path/sensors_class.ko
-insmod $module_path/utags.ko
-insmod $module_path/exfat.ko
-insmod $module_path/mmi_annotate.ko
-insmod $module_path/mmi_info.ko
-insmod $module_path/mmi_sys_temp.ko
-insmod $module_path/moto_f_usbnet.ko
-insmod $module_path/qpnp-power-on-mmi.ko
-insmod $module_path/qpnp_adaptive_charge.ko
-
-# Fix up chipone
-if [[ -d /sys/class/touchscreen/ICNL9911S ]]; then
+wait_for_poweron()
+{
+	local wait_nomore
+	local readiness
+	local count
+	wait_nomore=60
+	count=0
+	while true; do
+		readiness=$(cat $touch_path/poweron)
+		if [ "$readiness" == "1" ]; then
+			break;
+		fi
+		count=$((count+1))
+		[ $count -eq $wait_nomore ] && break
+		sleep 1
+	done
+	if [ $count -eq $wait_nomore ]; then
+		return 1
+	fi
+	return 0
+}
+cd $firmware_path
+touch_product_string=$(ls $touch_class_path)
+if [[ -d /sys/class/touchscreen/ft8006s_aa ]]; then
+       echo "focaltech"
+       firmware_file="focaltech-dsbj-ft8006s_aa-05-0000-cebu.bin"
+       touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk '{print $1}')
+       wait_for_poweron
+       echo $firmware_file > $touch_path/doreflash
+       echo 1 > $touch_path/forcereflash
+       sleep 5
+       echo 1 > $touch_path/reset
+elif [[ -d /sys/class/touchscreen/ICNL9911C ]]; then
         echo "chipone"
         chipone_fw_path=/vendor/firmware/ICNL9911.bin
         flash_path=/sys/chipone-tddi/cts_firmware
-
         sleep 2
-
         echo $chipone_fw_path > $flash_path/update_from_file
 fi
 
-exit 0
+return 0
